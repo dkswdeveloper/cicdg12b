f929cb8599dfd660f67fde09cb9ea6d1e301685c
Dinesh Kumar (C) <dinesh_budhiraja@yahoo.com>
2024-05-01 10:09:07 +0530

    npm init done, added lib for email

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+//create a new repo
+// add .readme .gitignore files
+// copy the url
+// create a new folder on your machine
+// git clone https://github.com/username/repo.git 
+// open the project in vscode (cd repofolder)
+// npm init -y
+// add new file app.js with some console statements
+// npm i 
+// git add .
+// git commit -m "message to add file"
+// git push
+
 console.log("server will here");
 console.log(new Date());
